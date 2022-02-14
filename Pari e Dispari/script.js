896843af66b6e84499dc6860467f9158177703f8
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-14 17:22:54 +0100

    Aggiunto esercizio pari e dispari

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -8,3 +8,56 @@
 // Dichiariamo chi ha vinto.
 
 
+let numUser;
+
+const pariDispari = prompt('Pari o dispari?');
+
+console.log(pariDispari);
+
+while ( isNaN(numUser) ) {
+
+    numUser = parseInt( prompt('Inserisci un numero da 1 a 5') );
+
+}
+
+console.log(numUser);
+
+let pari = false;
+
+function getRandomIntInclusive(min, max) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+
+}
+
+const numPc = getRandomIntInclusive(1,5);
+console.log(numPc);
+
+function sum (num1, num2) {
+
+    somma = num1 + num2;
+
+    if ( somma % 2 == 1) {
+        pari = false;
+    } else {
+        pari = true;
+    }
+
+    return somma;
+
+}
+
+const totNum = sum(numUser, numPc);
+console.log(totNum);
+
+if ( pari == true && pariDispari =='pari') {
+    console.log('Hai vinto!');
+} else if (pari == true && pariDispari =='dispari') {
+    console.log('Hai perso');
+} else if (pari == false && pariDispari =='dispari') {
+    console.log ('Hai vinto!');
+} else if (pari == false && pariDispari =='pari'){
+    console.log('Hai perso');
+}
