e94787b4fc65e0b4a1bbac3f7852135ec6c07d63
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-14 16:27:51 +0100

    Aggiunto esercizio palindroma

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -4,3 +4,25 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 
+const parola = prompt('Inserisci una parola');
+console.log(parola);
+
+function invertiParola(str) {
+
+    if (str !== "") {
+        return invertiParola(str.substr(1)) + str.charAt(0);
+    } else {
+        return "";
+    }
+
+} 
+
+console.log(invertiParola(parola));
+
+const parolaInv = invertiParola(parola);
+
+if( parolaInv == parola ){
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+}
